04c6209905cd6f501506ee24fdc17d91c8738acc
Kush Mahajan <mahajankush2803@gmail.com>
2025-01-21 00:23:33 +0530

    Basic Layout

---
+++ components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center bg-black text-white w-full px-3 py-3">
+      <div>This is footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-center bg-black text-white w-full px-3 py-3">
+      <div>This is header</div>
+    </header>
+  );
+};
+
+export default Header;
