70b13540da34feaf8b7e548279c3984deced2224
Kush Mahajan <mahajankush2803@gmail.com>
2025-01-22 19:43:30 +0530

    Test

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import clientPromise from "@/lib/mongodb";
 
-const page = () => {
+export default async function ProfilePage() {
+  const client = await clientPromise;
+  const db = client.db("test");
+  const users = await db.collection("users").find({}).toArray();
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <h1>Users List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default page
