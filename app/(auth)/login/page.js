f56c84bda8d05ff121ae550c8410d036c2b5710c
Divyanshverma1000 <divyanshverma05440@gmail.com>
2025-01-25 00:06:24 +0530

    toastify done and logout redirects to login

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,11 +1,13 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation"; // Use this instead of "next/router" for app router
+import { useRouter ,useSearchParams} from "next/navigation";
 import Header from "@/components/auth/Header";
 import LoginForm from "@/components/auth/LoginForm";
 import OTPForm from "@/components/auth/OTPForm";
 import LogoutMessage from "@/components/auth/LogoutMessage";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginPage = () => {
   const [step, setStep] = useState(1);
@@ -13,13 +15,18 @@ const LoginPage = () => {
   const [logoutMessage, setLogoutMessage] = useState("");
   const router = useRouter();
 
+  const searchParams = useSearchParams();
+
   useEffect(() => {
-    // Ensure this runs only in the client environment
-    if (typeof window !== "undefined" && router.query?.logout === "success") {
-      setLogoutMessage("You have been logged out successfully.");
-      router.replace("/", { shallow: true }); // Remove query param from URL
+    // toast.configure();  // Ensure toast is configured
+    const message = searchParams.get('message');
+    console.log("Query message:", message); 
+  
+    if (message === 'logout-success') {
+      toast.success('You have been logged out successfully.');
+      router.replace('/login'); // Clean up URL
     }
-  }, [router]);
+  }, [searchParams]);
 
   const handleNext = (enteredEmail) => {
     setEmail(enteredEmail);
@@ -32,27 +39,10 @@ const LoginPage = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        backgroundColor: "#f5f5f5",
-        fontFamily: "Arial, sans-serif",
-        overflow: "hidden",
-      }}
-    >
-      <Header />
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "500px",
-          textAlign: "center",
-          paddingTop: logoutMessage ? "30px" : "0",
-        }}
-      >
+    <div className="min-h-screen w-9/10 mx-4 rounded-2xl bg-gray-900 text-white flex items-center justify-center">
+       <ToastContainer />
+      <div className="w-full max-w-lg bg-gray-800 text-center py-6 px-4 rounded-2xl">
+        <Header />
         {logoutMessage && <LogoutMessage message={logoutMessage} />}
         {step === 1 ? (
           <LoginForm onNext={handleNext} />
